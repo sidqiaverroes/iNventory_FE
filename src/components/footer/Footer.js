9bcdeeaa446b31ad72b82d8b618b91ab0c4a1402
AriqAryasuta <ariqaryasuta3@gmail.com>
2022-11-16 22:23:54 +0700

    Feat: Add Footer, Header, and InfoBox

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="--flex-center --py2">
+      <p style={{ fontSize: "12px" }}>PAW Patrol 10 &copy; 2022</p>
+    </div>
+  );
+};
+
+export default Footer;
